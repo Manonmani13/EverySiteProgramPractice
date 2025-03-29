79196394e8b44a92b8dc93c25f48e87b8ba985b2
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-03-29 20:07:27 +0530

    example for why spring come

---
+++ ExampleForSpringboot/src/com/example/PetrolEngine.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class PetrolEngine  implements IEngine{
+
+	@Override
+	public int start() {
+		// TODO Auto-generated method stub
+		System.out.println("Diesel Engine started....");
+		return 0;
+	}
+
+}
