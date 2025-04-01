24e334b0e0602b7636b769c7c46be46ebea3e779
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-04-01 21:04:35 +0530

    changeS

--- 01-IOC-DI-App/src/main/java/App.java
+++ 01-IOC-DI-App/src/main/java/App.java
@@ -2,6 +2,8 @@
 public class App {
 	public static void main(String[] args) {
 		ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml"); 
+		Car car=context.getBean(Car.class);
+		car.drive();
 	}
 
 }
