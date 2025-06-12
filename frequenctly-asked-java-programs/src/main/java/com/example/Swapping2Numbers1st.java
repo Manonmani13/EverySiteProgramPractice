e378ea4a6f160d7ff07f3acb8566500b0173fa34
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-06-12 20:56:36 +0530

    swapp

---
+++ frequenctly-asked-java-programs/src/main/java/com/example/Swapping2Numbers1st.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class Swapping2Numbers1st {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        //Logic 1
+        int t=a;
+        a=b;
+        b=t;
+
+        //logic 2
+        a=a+b;
+        b=a-b;
+        a=a-b;
+
+        //logic 3
+        a=a*b;
+        b=a/b;
+        a=a/b;
+    }
+    
+}
