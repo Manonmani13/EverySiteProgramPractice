c12fbf1b7b0007079bc39fbbef6f7ff86f1697a0
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-06-22 12:09:56 +0530

    exmaples

---
+++ ExampleForSpringboot/src/com/example/Car.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Car {
+
+	private IEngine eng;
+	//constructor engine
+//	public Car(IEngine eng) {
+//		this.eng=eng;
+//	}
+//	
+//	//setter engine
+//	public void setEng(IEngine eng) {
+//		this.eng=eng;
+//	}
+	
+	public void drive() {
+		int drive=eng.start();
+		if(drive>=1) {
+			System.out.println("Journey Started ......");
+		}else {
+			System.out.println("Engine in trouble.....");
+		}
+	}
+}
