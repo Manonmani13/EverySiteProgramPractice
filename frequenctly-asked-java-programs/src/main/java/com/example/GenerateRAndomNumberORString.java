00f78bcba8cbd6aaf829a1220da932b33bb25d42
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-06-28 13:02:53 +0530

    random no

---
+++ frequenctly-asked-java-programs/src/main/java/com/example/GenerateRAndomNumberORString.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import java.util.Random;
+
+public class GenerateRAndomNumberORString {
+    public static void main(String[] args) {
+        Random rand=new Random();
+        System.out.println(rand.nextInt(10));
+
+        //logic 2
+        System.out.println(Math.random());
+
+    }
+}
