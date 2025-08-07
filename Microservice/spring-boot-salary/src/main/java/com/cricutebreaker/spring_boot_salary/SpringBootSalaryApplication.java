7f68d1a0c484e5af4d19d666b5553c7296fd1d6c
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-08-07 21:52:26 +0530

    salary

---
+++ Microservice/spring-boot-salary/src/main/java/com/cricutebreaker/spring_boot_salary/SpringBootSalaryApplication.java
@@ -0,0 +1,13 @@
+package com.cricutebreaker.spring_boot_salary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSalaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSalaryApplication.class, args);
+	}
+
+}
---
+++ Microservice/spring-boot-salary/src/test/java/com/cricutebreaker/spring_boot_salary/SpringBootSalaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricutebreaker.spring_boot_salary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSalaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
