03d48a6696b47f67c36e1c4eadd05efd7a0d3aca
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-08-26 20:36:46 +0530

    changes

---
+++ SpringSecurity/security1-example/src/test/java/com/security/security1_example/Security1ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.security1_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security1ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
