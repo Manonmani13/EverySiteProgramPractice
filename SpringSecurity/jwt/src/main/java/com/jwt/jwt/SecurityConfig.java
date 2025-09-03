a2d2fa117b6047749781fc7ec677959ac19b0e76
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-09-03 21:38:21 +0530

    jwt

---
+++ SpringSecurity/jwt/src/main/java/com/jwt/jwt/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.jwt.jwt;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+}
---
+++ SpringSecurity/jwt/src/main/java/com/jwt/jwt/UserController.java
@@ -0,0 +1,13 @@
+package com.jwt.jwt;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class UserController {
+    
+}
