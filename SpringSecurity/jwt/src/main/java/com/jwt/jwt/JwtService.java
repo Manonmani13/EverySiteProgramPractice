0cfdcef6a21d13ff1538d2ce792d7ee0dc109efc
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-11-08 10:16:17 +0530

    learnings

---
+++ SpringSecurity/jwt/src/main/java/com/jwt/jwt/JwtService.java
@@ -0,0 +1,33 @@
+package com.jwt.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+public class JwtService {
+    public static final String SECRET = "5367566859703373367639792F423F452848284D6251655468576D5A71347437";
+
+    public String generateToken(String email) { // Use email as username
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, email);
+    }
+
+    private String createToken(Map<String, Object> claims, String email) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
